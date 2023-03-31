b44626c5ad79df3abd898de2267eb6d6078be3e8
Roman <romka008@yandex.ru>
2023-03-31 12:37:51 +0300

    Изменен импорт класса SignUp

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import render from "./utils/renderDOM";
 
 import {HomePage} from "./pages/Home";
 import {Login} from "./pages/Login";
-import {SignUp} from "./pages/SignUp";
+import {SignUp} from "./pages/Signup";
 import {PageNotFound} from "./pages/PageNotFound";
 import {ErrorServer} from "./pages/ErrorServer";
 import {Profile} from "./pages/Profile";
